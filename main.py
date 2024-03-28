1e4ad23c133628cfe29361a5db202ac21cfaafad
Vsp4rks <155023824+Vsp4rks@users.noreply.github.com>
2024-03-28 17:34:12 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+name = input("Type in your name here: ")
+
+def displaymenu():
+    print("\n Welcome to the banking app, " + name + "!")
+    print()
+    print("Choose one of the following options:")
+    print("1. Check your balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Create Account")
+    print("5. Modify Account")
+    print("6. Delete Account")
+
