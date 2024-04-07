93f2c3bc4ebcfe3525582d703687b83b83e98e95
Vsp4rks <155023824+Vsp4rks@users.noreply.github.com>
2024-04-07 16:09:43 -0500

    adding in functions
    I made the functions in a different app and now im pasting them in and editing them where I see fit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import mysql.connector
+
+
+connection = mysql.connector.connect(
+      user = "root",
+      database = "abcdefg",
+      password = "g01108PF!"
+)
+
+cursor = connection.cursor()
+
+
+def checkbalance():
+    cursor.execute("SELECT balance FROM users WHERE username = %s", (username,))
