b16630c00f8a97ccd28452137cf86bfc7201a085
Vsp4rks <155023824+Vsp4rks@users.noreply.github.com>
2024-04-15 18:06:35 -0500

    trying to make the database work
    im putting in all of the functions, but i dont thing vsc is actually connected to mysql because it doesnt recognize some of what im doing

--- main.py
+++ main.py
@@ -14,12 +14,18 @@ account = None
 
 
 def checkbalance():
-    cursor.execute(f"SELECT Balance FROM bankdata WHERE id = {account[1]}")
+    cursor.execute(f"SELECT Balance FROM bankdata WHERE id = {account[0]}")
     result = cursor.fetchone()
-    return queryData[1]
+    return queryData[0]
 
 def deposit(amt):
     os.system("cls")
-    cursor.execute (f"UPDATE bankdata SET Balance = Balance + {amt} WHERE ID = '{account[1]}'")
+    cursor.execute (f"UPDATE bankdata SET Balance = Balance + {amt} WHERE ID = '{account[0]}'")
     connection.commit()
     print(f"${amt} deposited successfully!")
+
+
+
+#database close
+cursor.close
+connection.close
