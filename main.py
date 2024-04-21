19b1ae57371bd56b2deca6f23b820427333c6cbe
Vsp4rks <155023824+Vsp4rks@users.noreply.github.com>
2024-04-21 16:25:01 -0500

    fixing errors

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import os
 import mysql.connector
 
-
-def main():
- connection = mysql.connector.connect(
+connection = mysql.connector.connect(
       user = "root",
       database = "abcdefg",
       password = "g01108PF!"
 )
 
- cursor = connection.cursor()
+cursor = connection.cursor()
 
- account = None
-
+account = None
+def main():
  #important stuff goes below this comment
 
  def checkbalance():
     os.system("cls")
-    cursor.execute(f"SELECT Balance FROM bankdata WHERE id = {account[0]}")
+    cursor.execute(f"SELECT Balance FROM bankdata WHERE ID = {account[1]}")
     queryData = cursor.fetchone()
     return queryData[0]
 
  def deposit(amt):
     os.system("cls")
-    cursor.execute (f"UPDATE bankdata SET Balance = Balance + {amt} WHERE ID = '{account[0]}'")
+    cursor.execute (f"UPDATE bankdata SET Balance = Balance + {amt} WHERE ID = '{account[1]}'")
     connection.commit()
     print(f"${amt} deposited successfully!")
 
@@ -45,25 +43,25 @@ def main():
   os.system("cls")
   print("How much would you like to withdraw?: ")
   withdraw_amt = float(input("> "))
-   if withdraw_amt <= 0:
+  if withdraw_amt <= 0:
       print("Invalid amount!")
       return
-   if withdraw_amt > checkbalance():
+  if withdraw_amt > checkbalance():
       print("Insufficient funds!")
       return
-   cursor.execute (f"UPDATE bankdata SET Balance = Balance - {withdraw_amt} WHERE ID = '{account[0]}'")
-   connection.commit()
-   print(f"${withdraw_amt} withdrawn successfully!")
+  cursor.execute (f"UPDATE bankdata SET Balance = Balance - {withdraw_amt} WHERE ID = '{account[1]}'")
+  connection.commit()
+  print(f"${withdraw_amt} withdrawn successfully!")
   
  def accountdelete():
-    cursor.execute(f"DELETE FROM bankdata WHERE ID = {account[0]}")
+    cursor.execute(f"DELETE FROM bankdata WHERE ID = {account[12]}")
     connection.commit()
     print("Account deleted successfully!")
 
 
  while True:
    
-   answer = int(input("Good Afternoon! What would you like to do today?\n\n1: Login\n 2: Sign up\n"))
+   answer = int(input("Good Afternoon! What would you like to do today?\n\n1: Login\n2: Sign up\n"))
     
    if answer == 1: #login
       username = input("Enter your username: ")
@@ -78,13 +76,14 @@ def main():
          break
       os.system("cls")
       print("Not an existing user. Please try again.\n")
-   elif answer == 1: #sign up
+   elif answer == 2: #sign up
+        ID = input("Enter your ID: ")
         username = input("Enter your username: ")
         password = input ("Enter your password: ")
         balance = input("Enter your balance: ")
-        values = (username, password, balance)
-        mySql_insert_query = (f"INSERT INTO bankdata (Username, Password, Balance) VALUES (%s, %s, %s)")
-        cursor.execute(mySql_insert_query, values)
+        values = (ID, username, password, balance)
+        mySql_insert_query = (f"INSERT INTO bankdata (ID, Username, Password, Balance) VALUES (%s, %s, %s, %s)")
+        cursor.execute(mySql_insert_query, values) #error here
         connection.commit()
         print("Registration Successful!")
         account = cursor.fetchone()
@@ -108,8 +107,8 @@ def main():
 
 
  #database close
- cursor.close
- connection.close
+cursor.close
+connection.close
 
 if __name__ == "__main__":
   main()
